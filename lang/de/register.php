331005056a620feb87fb203bfbba2ae11fe08c11
flaviotecnologia2u <flavio@tecnologia2u.com.br>
2025-01-05 02:42:28 +0400

    register lang update + questionnaire update with lang

---
+++ lang/de/register.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+   'register_with' => 'Registrieren mit',
+'name' => 'Vorname',
+'last_name' => 'Nachname',
+'country' => 'Land',
+'city' => 'Stadt',
+'username' => 'Benutzername',
+'phone' => 'Telefon (nur Zahlen)',
+'email' => 'E-Mail',
+'password' => 'Passwort',
+'confirm_password' => 'Passwort bestätigen',
+'questionnaire' => 'Fragebogen',
+'register' => 'Registrieren',
+'next' => 'Weiter',
+
+'q5' => 'Im Jahr 2025 möchte ich zusätzlich passives Einkommen erzielen in Höhe von:',
+'q6' => 'Mit diesem zusätzlichen Geld möchte ich mein Leben verbessern und folgendes kaufen:',
+'q7' => 'Wenn sich die richtige Gelegenheit ergibt, bin ich bereit, X Personen zu rekrutieren:',
+
+    
+    
+];
---
+++ lang/ru/register.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'register_with' => 'Register With',
+    'name' => 'First Name',
+    'last_name' => 'Last Name',
+    'country' => 'Country',
+    'city' => 'City',
+    'username' => 'Username',
+    'phone' => 'Phone (Just Numbers)',
+    'email' => 'Email',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'questionnaire' => 'Questionnaire',
+    'register' => 'Register',
+    'next' => 'Next',
+
+    'q5'=>'In 2025, I would like to make extra passive income of:',
+    'q6'=>'Using this additional money, I wish to improve my life and purchase:',
+    'q7'=>'If given the right opportunity, I’m willing to recruit X people:',
+    
+    
+];
