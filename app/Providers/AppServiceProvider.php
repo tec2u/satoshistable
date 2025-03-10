732e3e3877040c7efce061c0dadef1c6b0d8b6d4
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2025-03-10 07:47:53 -0300

    att language

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,10 +15,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-
-    }
+    public function register() {}
 
     /**
      * Bootstrap any application services.
@@ -25,5 +25,35 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        } else {
+            // Obtém o locale do IP do usuário
+            $locale = $this->getUserLocale();
+            Session::put('locale', $locale);
+            App::setLocale($locale);
+        }
+    }
+
+    private function getUserLocale()
+    {
+        $ip = Request::ip();
+
+        $countryCode = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"))->countryCode ?? 'US';
+
+        $locales = [
+            'US' => 'en', // Inglês
+            'GB' => 'en', // Inglês
+            'ES' => 'es', // Espanhol
+            'BR' => 'br', // Português Brasil
+            'DE' => 'de', // Alemão
+            'FR' => 'fr', // Francês
+            'CN' => 'ch', // Chinês
+            'IN' => 'in', // Indiano
+            'RU' => 'ru', // Russo
+        ];
+
+        return $locales[$countryCode] ?? 'en';
     }
 }
