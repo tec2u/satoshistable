9a7fdddd92ea083ba75f58764b90e1ba7aca4e2b
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2025-03-14 18:03:54 -0300

    att

--- resources/views/package/packagepay.blade.php
+++ resources/views/package/packagepay.blade.php
@@ -174,9 +174,7 @@
         </div>
     </section>
 </main>
-{{$wallet}}
-{{$wallet->address}}
-{{$orderpackage->payment_status}}
+
 @if (isset($wallet) && isset($wallet->address) && $orderpackage->payment_status != 2)
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
