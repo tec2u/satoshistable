807bcef5963c3567a51ac02a6a4ba9aa63fdb151
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2025-04-11 10:44:54 -0300

    att

--- database/migrations/2025_04_10_175148_add_column_theme_id_in_users_table.php
+++ database/migrations/2025_04_10_175148_add_column_theme_id_in_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('theme_id')->nullable();
+            $table->unsignedBigInteger('theme_id')->default(1);
         });
     }
 
