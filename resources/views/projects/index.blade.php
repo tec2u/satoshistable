c0427dceaa1c5a97085dc3c89a8b136e92bfc5b0
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2025-04-14 08:28:38 -0300

    att

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,30 +6,60 @@
         <div class="fade">
             <div class="container-fluid">
                 <h1>@lang('package.products')</h1>
-                <div class="">
-                    <div class="row">
-                        @forelse($projects as $project)
-                        <div class="row">
-                            <div class="col-3 d-flex align-items-center justify-content-center flex-column">
-                                <img src="{{ asset($project->logo) }}" alt="" class="logo-project">
+                <div class="row d-flex flex-wrap">
+                    @forelse($projects as $project)
+                    <div class="card w-375px shadow-sm mr-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <!-- Logo e nome do projeto -->
+                                <div class="d-flex align-items-center justify-content-center flex-column text-center">
+                                    <img src="{{ asset($project->logo) }}" alt="Logo" class="logo-project mb-3" style="max-width: 100px;">
+                                    <h5 class="card-title">{{ $project->name }}</h5>
+                                </div>
+
+                                <!-- Link de registro -->
                                 <div>
-                                    <h2 class="my-4">{{ $project->name }}</h2>
+                                    <div class="d-flex flex-column align-items-start">
+                                        <label class="form-label">Register:</label>
+                                        <div class="input-group">
+                                            <input type="text" class="input_st w-50" readonly id="register_{{ $project->id }}" value="{{ route('indication', ['project_id' => $project->id, 'id' => auth()->user()->id]) }}">
+                                            <div>
+                                                <button class="btn btn-outline-primary btn-background-color" type="button" onclick="copyLink('register_{{ $project->id }}')">copy</button>
+                                                <button class="btn btn-outline-primary btn-background-color" type="button" onclick="copenLink'register_{{ $project->id }}')">open</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Landing page -->
+                                    <div class="d-flex flex-column align-items-start">
+                                        <label class="form-label">Landing page:</label>
+                                        <div class="input-group">
+                                            <input type="text" class="input_st w-50" readonly id="landing_{{ $project->id }}" value="{{ route('landing', ['project_id' => $project->id, 'id' => auth()->user()->id]) }}">
+                                            <div>
+                                                <button class="btn btn-outline-primary btn-background-color" type="button" onclick="copyLink('landing_{{ $project->id }}')">copy</button>
+                                                <button class="btn btn-outline-primary btn-background-color" type="button" onclick="openLink('landing_{{ $project->id }}')">open</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Perfil do projeto -->
+                                    <div class="d-flex flex-column align-items-start">
+                                        <label class="form-label">Project profile:</label>
+                                        <div class="input-group">
+                                            <input type="text" class="input_st w-50" readonly id="profile_{{ $project->id }}" value="{{ route('projects.profile', ['id' => $project->id]) }}">
+                                            <div>
+                                                <button class="btn btn-outline-primary btn-background-color" type="button" onclick="copyLink('profile_{{ $project->id }}')">copy</button>
+                                                <button class="btn btn-outline-primary btn-background-color" type="button" onclick="openLink('profile_{{ $project->id }}')">open</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3 d-flex align-items-center">
-                                <label for="" class="mb-0 mr-2">Register: </label><input type="text" class="input_st" readonly id="register_{{ $project->id }}" value="{{ route('indication', ['project_id' => $project->id, 'id' => auth()->user()->id]) }}"> <button class="btn btn-outline-primary btn-small" onclick="copyLink('register_{{ $project->id }}')">Copy</button>
-                            </div>
-                            <div class="col-3 d-flex align-items-center">
-                                <label for="" class="mb-0 mr-2">Landing page: </label><input type="text" class="input_st" readonly id="landing_{{ $project->id }}" value="{{ route('landing', ['project_id' => $project->id, 'id' => auth()->user()->id]) }}"> <button class="btn btn-outline-primary btn-small" onclick="copyLink('landing_{{ $project->id }}')">Copy</button>
-                            </div>
-                            <div class="col-3 d-flex align-items-center">
-                                <label for="" class="mb-0 mr-2">Project profile: </label><input type="text" class="input_st" readonly id="profile_{{ $project->id }}" value="{{ route('projects.profile', ['id' => $project->id]) }}"> <button class="btn btn-outline-primary btn-small" onclick="copyLink('profile_{{ $project->id }}')">Copy</button>
-                            </div>
                         </div>
-                        @empty
-                        <p>@lang('package.any_products_registered')</p>
-                        @endforelse
                     </div>
+                    @empty
+                    <p>@lang('package.any_products_registered')</p>
+                    @endforelse
                 </div>
             </div>
         </div>
@@ -45,23 +75,46 @@
     }
 
     .input_st {
+        background-color: transparent;
         border: none;
+        font-size: 8px;
     }
 
     .input_st::placeholder {
         background-color: transparent;
     }
+
+    .w-375px {
+        width: 375px;
+    }
+    .w-50 {
+        width: 50%;
+    }
+
+    .w-30 {
+        width: 30%;
+    }
+
+    .input-group {
+        justify-content: space-between;
+    }
 </style>
 <script>
- function copyLink(id) {
+    function copyLink(id) {
 
-    var copyText = document.getElementById(id);
-    console.log(copyText)
+        var copyText = document.getElementById(id);
+        console.log(copyText)
 
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
 
-    navigator.clipboard.writeText(copyText.value);
-}
+        navigator.clipboard.writeText(copyText.value);
+    }
+
+    function openLink(id) {
+
+        var copyText = document.getElementById(id);
+        window.open(copyText.value)
+    }
 </script>
 @endsection
